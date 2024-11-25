4c635e5f7b5dc61f7dc8a8cfc36aba624ecbc149
akhilaravindsb@gmail.com <akhilaravindsb@gmail.com>
2024-11-25 19:16:10 +0530

    aAdded notification details as input to the details component

--- src/app/notification-search/notification-details/notification-details.component.ts
+++ src/app/notification-search/notification-details/notification-details.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
+interface eventType{
+  caseId: string,
+  miniTeam: string,
+  name: string,
+  dob: string,
+  gender: string,
+  ssn: string
+}
 @Component({
   selector: 'notification-details',
   templateUrl: './notification-details.component.html',
   styleUrl: './notification-details.component.css'
 })
 export class NotificationDetailsComponent {
+  @Input() notification:eventType | null = null;
 
 }
+
--- src/app/notification-search/notification-search.component.ts
+++ src/app/notification-search/notification-search.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrl: './notification-search.component.css'
 })
 export class NotificationSearchComponent {
-
+    notifficationDetails = {
+      caseId:'5507744.2',
+      miniTeam:'310AB',
+      name:'Last Name, First Name',
+      dob:'12/05/1965',
+      gender:'m',
+      ssn:'XXXX-XX-9999'
+    }
 }
